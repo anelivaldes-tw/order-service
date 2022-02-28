a636f6f177cb8dc6286bec2899b265e1e0d95fcf
Aneli Valdés <aneli.valdes@thoughtworks.com>
2022-02-28 18:46:58 -0300

    Add | Migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,13 @@ import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health/health.controller';
 import { HealthModule } from './health/health.module';
 import { mysql } from './sequelize.config';
+import { Order } from "./orders/models/orders.model";
 
 @Module({
   imports: [
     TerminusModule,
     HealthModule,
-    SequelizeModule.forRoot(mysql),
+    SequelizeModule.forRoot({...mysql, models: [Order]}),
     OrdersModule,
   ],
   controllers: [AppController, HealthController],
