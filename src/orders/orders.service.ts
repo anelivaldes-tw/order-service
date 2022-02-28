8b3174f9e2c00988daabd4f55fcc3441e8e6f3eb
Aneli Valdés <aneli.valdes@thoughtworks.com>
2022-02-28 18:48:15 -0300

    Add | sending and event after when an order is created

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -2,17 +2,27 @@ import { Injectable } from "@nestjs/common";
 import { Order } from "./models/orders.model";
 import { InjectModel } from "@nestjs/sequelize";
 import { CreateOrdersDto } from "./dto/create-orders.dto";
+import { Client, ClientKafka } from "@nestjs/microservices";
+import { microserviceConfig } from "../microserviceConfig";
 
 @Injectable()
 export class OrdersService {
+
+  @Client(microserviceConfig)
+  client: ClientKafka;
+
   constructor(
     @InjectModel(Order)
     private readonly orderModel: typeof Order
   ) {
   }
 
-  create(createOrdersDto: CreateOrdersDto): Promise<Order> {
-    return this.orderModel.create<Order>({ state: "PENDING", ...createOrdersDto });
+  async create(createOrdersDto: CreateOrdersDto): Promise<Order> {
+    const order = await this.orderModel.create<Order>({ state: "PENDING", ...createOrdersDto });
+    if(order.id){
+      this.client.emit<string>('orders', JSON.stringify(order));
+    }
+    return order;
   }
 
   async findAll(): Promise<Order[]> {
