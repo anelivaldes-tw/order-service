38f73764ce5a5b9b2c61da88ca7b63bcd2b709a5
Aneli Valdés <aneli.valdes@thoughtworks.com>
2022-03-04 12:12:23 -0300

    refactor | cleaning code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { OrdersModule } from './orders/orders.module';
-import { TerminusModule } from '@nestjs/terminus';
-import { HealthController } from './health/health.controller';
-import { HealthModule } from './health/health.module';
-import { EventPublisherModule } from './event-publisher/event-publisher.module';
-import { EventHandlerModule } from './event-handler/event-handler.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { OrdersModule } from "./orders/orders.module";
+import { EventPublisherModule } from "./event-publisher/event-publisher.module";
+import { EventHandlerModule } from "./event-handler/event-handler.module";
 import { ConfigModule } from "@nestjs/config";
 import { DatabaseModule } from "./database/database.module";
 
@@ -14,13 +11,12 @@ import { DatabaseModule } from "./database/database.module";
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
-    TerminusModule,
-    HealthModule,
     OrdersModule,
     EventPublisherModule,
-    EventHandlerModule,
+    EventHandlerModule
   ],
-  controllers: [AppController, HealthController],
-  providers: [AppService],
+  controllers: [AppController],
+  providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+}
